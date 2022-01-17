35d63f7d61d9a09637c625ef4dd90f52fbc1dd3c
Sahil Verma <iamsahil1910@gmail.com>
2022-01-17 19:54:54 +0530

    Updates

---
+++ precache-manifest.0f41a95435e1b07c6891024324250005.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dda6ba4eca470dfa5241e52711084f9",
+    "url": "/healthcare-innovaccer/index.html"
+  },
+  {
+    "revision": "2397ee72fa1832f83c88",
+    "url": "/healthcare-innovaccer/static/css/main.2c35fcb5.chunk.css"
+  },
+  {
+    "revision": "0a5f56132e1992e6f3f8",
+    "url": "/healthcare-innovaccer/static/js/2.df38d390.chunk.js"
+  },
+  {
+    "revision": "b9a640effce7f794ec400623a8248713",
+    "url": "/healthcare-innovaccer/static/js/2.df38d390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2397ee72fa1832f83c88",
+    "url": "/healthcare-innovaccer/static/js/main.d56226d8.chunk.js"
+  },
+  {
+    "revision": "037f66785d8d4ab28ede",
+    "url": "/healthcare-innovaccer/static/js/runtime-main.6e03ba51.js"
+  },
+  {
+    "revision": "afe901424be3e61305cd326d21f57d5e",
+    "url": "/healthcare-innovaccer/static/media/3.afe90142.jpg"
+  },
+  {
+    "revision": "dc64366c60865c50740dd32d705ae0ff",
+    "url": "/healthcare-innovaccer/static/media/Mask Group 4.dc64366c.jpg"
+  },
+  {
+    "revision": "f2681bc71eb940a911f3b4d136f25b7e",
+    "url": "/healthcare-innovaccer/static/media/Mask Group 5.f2681bc7.jpg"
+  },
+  {
+    "revision": "2ac90daf67878c29a388e022904101db",
+    "url": "/healthcare-innovaccer/static/media/drimage.2ac90daf.jpg"
+  },
+  {
+    "revision": "7e5aec06294edf5718bd75237dac47f7",
+    "url": "/healthcare-innovaccer/static/media/footer.7e5aec06.png"
+  },
+  {
+    "revision": "e12239faeb87763a6eab1595993723e2",
+    "url": "/healthcare-innovaccer/static/media/patient.e12239fa.jpg"
+  }
+]);
